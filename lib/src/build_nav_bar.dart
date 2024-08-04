8b12b4c2ee2a8afa5820e1d7dab38ab0a4f1d43a
Narges <ijef.sonper@gmail.com>
2024-08-04 13:34:01 +0330

    change navbar

--- lib/src/build_nav_bar.dart
+++ lib/src/build_nav_bar.dart
@@ -111,22 +111,19 @@ class _WaterDropNavBarState extends State<WaterDropNavBar>
                 children: items.map(
                   (BarItem item) {
                     final int index = items.indexOf(item);
-                    return Container(
-                      color: Colors.black,
-                      child: BuildIconButton(
-                        bottomPadding: bottomPadding,
-                        barHeight: barHeight,
-                        barColor: backgroundColor,
-                        inactiveColor: inactiveIconColor,
-                        color: dropColor,
-                        index: index,
-                        iconSize: iconSize,
-                        seletedIndex: selectedIndex.toInt(),
-                        controller: _controller,
-                        selectedIcon: item.filledIcon,
-                        unslectedIcon: item.outlinedIcon,
-                        onPressed: () => _onTap(index),
-                      ),
+                    return BuildIconButton(
+                      bottomPadding: bottomPadding,
+                      barHeight: barHeight,
+                      barColor: backgroundColor,
+                      inactiveColor: inactiveIconColor,
+                      color: dropColor,
+                      index: index,
+                      iconSize: iconSize,
+                      seletedIndex: selectedIndex.toInt(),
+                      controller: _controller,
+                      selectedIcon: item.filledIcon,
+                      unslectedIcon: item.outlinedIcon,
+                      onPressed: () => _onTap(index),
                     );
                   },
                 ).toList(),
